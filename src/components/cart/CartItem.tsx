086598edbeab86dee449465b0e5a460beb336282
Mateusz <m.wesolowski.92@gmail.com>
2025-05-25 22:42:12 +0200

    Create cart components with static data

---
+++ src/components/cart/CartItem.tsx
@@ -0,0 +1,23 @@
+import type { CartItemTypes } from "../../types/types";
+import { formatCurrency } from "../../utils/helpers";
+import { Button } from "../ui/Button";
+
+interface Props {
+  item: CartItemTypes;
+}
+
+export const CartItem = ({
+  item: { pizzaId, name, quantity, totalPrice },
+}: Props) => {
+  return (
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
+        {quantity}&times; {name}
+      </p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
+      </div>
+    </li>
+  );
+};
