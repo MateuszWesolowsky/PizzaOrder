6e88ad239f1addcdfadf1dd78eb969fbdfb183a1
Mateusz <m.wesolowski.92@gmail.com>
2025-05-28 16:24:30 +0200

    Add if statement to display Button coponent

--- src/components/ui/Home.tsx
+++ src/components/ui/Home.tsx
@@ -1,6 +1,9 @@
+import { useAppSelector } from "../../hooks";
 import { CreateUser } from "../user/CreateUser";
+import { Button } from "./Button";
 
 export const Home = () => {
+  const userName = useAppSelector((state) => state.user.userName);
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8 text-xl font-semibold md:text-3xl">
@@ -10,7 +13,13 @@ export const Home = () => {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {!userName ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering, {userName}
+        </Button>
+      )}
     </div>
   );
 };
