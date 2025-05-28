8ff28e09d70c54febfffcad8052bce973cad2044
Mateusz <m.wesolowski.92@gmail.com>
2025-05-28 16:25:12 +0200

    Remove static data and display dynamic data from Redux

--- src/components/user/Username.tsx
+++ src/components/user/Username.tsx
@@ -1,3 +1,11 @@
+import { useAppSelector } from "../../hooks";
+
 export const Username = () => {
-  return <div className="hidden text-sm font-semibold md:block">Mateusz</div>;
+  const userName = useAppSelector((state) => state.user.userName);
+
+  if (!userName) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 };
