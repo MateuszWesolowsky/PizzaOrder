44eacaf0c0b7af4a797f9a8f3c2693eaf666f795
Mateusz <m.wesolowski.92@gmail.com>
2025-05-28 16:34:18 +0200

    Add dispatch to AddItemToCart and add logic to display buttons by currentPizzaQuantity selector

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -1,5 +1,8 @@
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import type { PizzaTypes } from "../../types/types";
 import { formatCurrency } from "../../utils/helpers";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import { DeleteItem } from "../cart/DeleteItem";
 import { Button } from "../ui/Button";
 
 interface PizzaListProps {
@@ -9,6 +12,23 @@ interface PizzaListProps {
 export const MenuItem = ({
   pizza: { id, name, unitPrice, ingredients, soldOut, imageUrl },
 }: PizzaListProps) => {
+  const dispatch = useAppDispatch();
+
+  const currentPizzaQuantity = useAppSelector(getCurrentQuantityById(id));
+
+  const isInCart = currentPizzaQuantity > 0;
+
+  const handleAddToCart = () => {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  };
+
   return (
     <li className="flex gap-4 py-2">
       <img
@@ -29,7 +49,12 @@ export const MenuItem = ({
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+          {isInCart && <DeleteItem pizzaId={id} />}
+          {!soldOut && !isInCart && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
