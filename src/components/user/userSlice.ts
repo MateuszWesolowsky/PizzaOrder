9d9f953938eed00f2f0d7586cc29bd300286ee21
Mateusz <m.wesolowski.92@gmail.com>
2025-05-28 16:22:13 +0200

    Create userSlice

---
+++ src/components/user/userSlice.ts
@@ -0,0 +1,47 @@
+// import { getAddress } from "../../services/apiGeocoding";
+
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+// const getPosition = () => {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+// const fetchAddress = async () => {
+//   // 1) We get the user's geolocation position
+//   const positionObj = await getPosition();
+//   const position = {
+//     latitude: positionObj.coords.latitude,
+//     longitude: positionObj.coords.longitude,
+//   };
+
+//   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+//   const addressObj = await getAddress(position);
+//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+//   // 3) Then we return an object with the data that we are interested in
+//   return { position, address };
+// };
+
+interface UserState {
+  userName: string;
+}
+
+const initialState: UserState = {
+  userName: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action: PayloadAction<string>) {
+      state.userName = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
