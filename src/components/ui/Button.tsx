25db0117671ae80b2508bc3f2ce47ee27f4fb9d2
Mateusz <m.wesolowski.92@gmail.com>
2025-06-05 10:51:40 +0200

    Update button props

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,13 +5,15 @@ type ButtonProps = {
   children: ReactNode;
   disabled?: boolean;
   to?: string;
-  type: "primary" | "small" | "secondary";
-  onClick?: () => void;
+  typeOfBtn: "primary" | "small" | "secondary" | "round";
+  type?: "submit" | "button";
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 export const Button = ({
   children,
   disabled,
   to,
+  typeOfBtn,
   type,
   onClick,
 }: ButtonProps) => {
@@ -21,25 +23,31 @@ export const Button = ({
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
     secondary:
       "cursor-pointer text-sm rounded-full border-2 border-stone-300 font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:text-stone-800 hover:bg-stone-300 focus:ring focus:text-stone-800 focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed inline-block px-4 py-2.5 md:px-6 md:py-3.5",
   };
 
   if (to)
     return (
-      <Link className={styles[type]} to={to}>
+      <Link className={styles[typeOfBtn]} to={to}>
         {children}
       </Link>
     );
   if (onClick)
     return (
-      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        className={styles[typeOfBtn]}
+        type={type}
+      >
         {children}
       </button>
     );
 
   return (
-    <button disabled={disabled} className={styles[type]}>
+    <button disabled={disabled} className={styles[typeOfBtn]} type={type}>
       {children}
     </button>
   );
